b73bba739b366ed8515e94123de0d67820fd040a
John Tian <johntian2014@gmail.com>
2022-07-05 20:19:42 -0500

    Update HandTrackingModule.py

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -48,8 +48,8 @@ def main():
     detector=handDetector()
     while True:
         success, img = cap.read()
-        img=detector.findHands(img,draw=False)
-        lmList=detector.findPosition(img,draw=False)
+        img=detector.findHands(img)
+        lmList=detector.findPosition(img)
         if len(lmList)!=0:
             print(lmList[4])
         cTime = time.time()
